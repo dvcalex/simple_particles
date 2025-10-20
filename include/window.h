daa785f667c0e4424933e69dc7b34d8d26fc978e
dvcalex <jacrisspi77@gmail.com>
2025-10-20 16:47:02 -0400

    setup window driven render loop with single callbacks for input and rendering

--- include/window.h
+++ include/window.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <string_view>
-
-class GLFWwindow;
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 class Window
 {
@@ -10,8 +10,24 @@ public:
 	Window(int width, int height, std::string_view title = "simple_particles");
 	~Window();
 
-	//GLFWwindow* getGLFWwindow() const { return window; }
-	void startWindowLoop();
+	GLFWwindow* getGLFWwindow() const { return window; }
+
+	template<typename InputCallback, typename RenderCallback>
+	void startUpdating(InputCallback inputCallback, RenderCallback renderCallback)
+	{
+		while (!this->shouldClose())
+		{
+			// Process input
+			inputCallback();
+
+			// Render
+			renderCallback();
+
+			// Swap buffers and poll events
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+		}
+	}
 
 private:
 	static void framebufferSizeCallback(GLFWwindow* window, int width, int height);
